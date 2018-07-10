1cb1ade02b07bd94ae8a301a55816e4798880315
Juan Bautista Mesa Rodlan <juan.mesa@hidup.io>
2018-07-10 14:06:33 +0200

    adding domain multi url

--- deploy/docker-entrypoint.sh
+++ deploy/docker-entrypoint.sh
@@ -1,5 +1,63 @@
-#!/bin/sh
+#!/bin/sh -x
 set -e
+
+function query () { echo ${HAPROXY_JSON_CFG} | jq "$1" | sed 's/"//g'; }
+function putfile () { echo "${1}" >> ${2}; }
+function putcrtlist () { putfile "$1" "/usr/local/etc/haproxy/crt-list.txt"; }
+function putcfg () { putfile "$1" "/usr/local/etc/haproxy/haproxy.cfg"; }
+function putpem () { putfile "$1" "$2"; }
+
+for crt in $(query '.certs[]')
+do
+  putcrtlist "${crt}"
+done
+
+for domain in $(query ".domains[].name")
+do
+  idx=0
+  for url in $(query '.domains | map(select(.name == "'$domain'"))[] | .url[]')
+  do
+    putcfg "  acl ${domain}_${idx}_acl hdr(host) -i ${url}"
+    putcfg "  use_backend ${domain}_backend if ${domain}_${idx}_acl"
+    idx=${idx}0
+  done
+done
+
+putcfg ""
+
+for userlist in $(query ".userlist[].name")
+do
+  putcfg "userlist ${userlist}"
+  for user in $(query '.userlist | map(select(.name == "'$userlist'" ))[].users[].login')
+  do
+    passtype=$(query '.userlist | map(select(.name == "'$userlist'"))[].users | map(select(.login == "'$user'"))[] | .passtype')
+    passfile=$(query '.userlist | map(select(.name == "'$userlist'"))[].users | map(select(.login == "'$user'"))[] | .passfile')
+    putcfg "  user ${user} ${passtype} $(cat ${passfile})"
+  done
+done
+
+putcfg ""
+
+for domain in $(query ".domains[].name")
+do
+  server=$(query '.domains | map(select(.name == "'$domain'"))[] | .server')
+  port=$(query '.domains | map(select(.name == "'$domain'"))[] | .port')
+  putcfg "backend ${domain}_backend
+  mode http
+  server ${domain}_srv ${server}:${port}"
+  userlist="$(query '.domains | map(select(.name == "'$domain'")) | map(select(.basicauth == true))[] | .userlist')"
+  [[ ! -z "${userlist}" ]] && {
+    putcfg "  acl AuthOkay_${domain} http_auth(${userlist})"
+    putcfg "  http-request auth realm Forbiden if !AuthOkay_${domain}"
+  }
+done
+
+[[ ! -z "${DEBUG}" ]] && {
+  echo ${HAPROXY_JSON_CFG}
+  cat /usr/local/etc/haproxy/haproxy.cfg
+}
+
+/sbin/syslogd -O /proc/1/fd/1
 
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
@@ -14,51 +72,4 @@ if [ "$1" = 'haproxy' ]; then
 	set -- "$(which haproxy-systemd-wrapper)" -p /run/haproxy.pid -db "$@"
 fi
 
-function putfile () { echo "${1}" >> ${2}; }
-function putcrtlist () { putfile "$1" "/usr/local/etc/haproxy/crt-list.txt"; }
-function putcfg () { putfile "$1" "/usr/local/etc/haproxy/haproxy.cfg"; }
-function putpem () { putfile "$1" "$2"; }
-
-for crt in $(echo ${HAPROXY_JSON_CFG} | jq ".certs[]" | sed 's/"//g')
-do
-  putcrtlist "${crt}"
-done
-
-for domain in $(echo ${HAPROXY_JSON_CFG} | jq ".domains[].name" | sed 's/"//g')
-do
-  url=$(echo ${HAPROXY_JSON_CFG} | jq '.domains | map(select(.name == "'$domain'"))[] | .url' | sed 's/"//g')
-  putcfg "  acl ${domain}_acl hdr(host) -i ${url}"
-  putcfg "  use_backend ${domain}_backend if ${domain}_acl"
-done
-
-putcfg ""
-
-for userlist in $(echo ${HAPROXY_JSON_CFG} | jq ".userlist[].name" | sed 's/"//g')
-do
-  putcfg "userlist ${userlist}"
-  for user in $(echo ${HAPROXY_JSON_CFG} | jq '.userlist | map(select(.name == "'$userlist'" ))[].users[].login' | sed 's/"//g')
-  do
-    passtype=$(echo ${HAPROXY_JSON_CFG} | jq '.userlist | map(select(.name == "'$userlist'"))[].users | map(select(.login == "'$user'"))[] | .passtype' | sed 's/"//g')
-    passfile=$(echo ${HAPROXY_JSON_CFG} | jq '.userlist | map(select(.name == "'$userlist'"))[].users | map(select(.login == "'$user'"))[] | .passfile' | sed 's/"//g')
-    putcfg "  user ${user} ${passtype} $(cat ${passfile})"
-  done
-done
-
-putcfg ""
-
-for domain in $(echo ${HAPROXY_JSON_CFG} | jq ".domains[].name" | sed 's/"//g')
-do
-  server=$(echo ${HAPROXY_JSON_CFG} | jq '.domains | map(select(.name == "'$domain'"))[] | .server' | sed 's/"//g')
-  port=$(echo ${HAPROXY_JSON_CFG} | jq '.domains | map(select(.name == "'$domain'"))[] | .port' | sed 's/"//g')
-  putcfg "backend ${domain}_backend
-  mode http
-  server ${domain}_srv ${server}:${port}"
-  userlist="$(echo ${HAPROXY_JSON_CFG} |  jq '.domains | map(select(.name == "'$domain'")) | map(select(.basicauth == true))[] | .userlist' | sed 's/"//g')"
-  [[ ! -z "${userlist}" ]] && {
-    putcfg "  acl AuthOkay_${domain} http_auth(${userlist})"
-    putcfg "  http-request auth realm Forbiden if !AuthOkay_${domain}"
-  }
-done
-
-/sbin/syslogd -O /proc/1/fd/1
 exec "$@"
